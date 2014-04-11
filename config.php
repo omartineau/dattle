a718036c435eb3c46be482640a8bb3828185cb99
leahpar@gmail.com <leahpar@gmail.com>
2014-04-12 01:36:44 +0200

    add twig

--- config.php
+++ config.php
@@ -1,11 +1,26 @@
 <?php
 #    define('MYSQL_DB','dattle');
-    include ('classes/question.class.php');
+include ('classes/question.class.php');
 
-    include('db.php');
-    try {
-        $con = new PDO( 'mysql:host='.MYSQL_HOST.';dbname='.MYSQL_DB.';port='.MYSQL_PORT.';', MYSQL_USER, MYSQL_PASS );
-    } catch ( Exception $e ) {
-        echo "Connection à MySQL impossible : ", $e->getMessage();
-        die();
-    }
+include('db.php');
+try {
+    $con = new PDO( 'mysql:host='.MYSQL_HOST.';dbname='.MYSQL_DB.';port='.MYSQL_PORT.';', MYSQL_USER, MYSQL_PASS );
+} catch ( Exception $e ) {
+    echo "Connection à MySQL impossible : ", $e->getMessage();
+    die();
+}
+
+
+
+// Load Twig
+require_once 'lib/Twig/Autoloader.php';
+Twig_Autoloader::register();
+
+// Init Twig
+$loader = new Twig_Loader_Filesystem('templates');
+$twig = new Twig_Environment($loader, array());
+
+// use
+// $template = $twig->loadTemplate('template.html.twig');
+// echo $template->render(array('name' => 'toto'));
+
